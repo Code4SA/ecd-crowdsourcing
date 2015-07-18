3ded859423ce2b4386bd7b45476a8339a9cbda8a
Adi Eyal <adi@burgercom.co.za>
2015-07-19 00:39:11 +0200

    Simplfied code a little
    Added csv files

--- address.py
+++ address.py
@@ -8,17 +8,11 @@ def gen_re():
     return re.compile(piped)
     
 
-def parse_address(x):
-    if re_place.search(x):
-        return x
-    return None
-
 re_place = gen_re()
-reader = csv.DictReader(open('ecd_npo.csv'))
-with open('output_npo.csv', 'w') as fp:
+reader = csv.DictReader(open('npo.csv'))
+with open('ecd_npo.csv', 'w') as fp:
     writer = csv.DictWriter(fp, field_names)
     for datum in reader:
         address = datum['physical_address']
-        result = parse_address(address)
-        if result:
+        if re_place.search(x):
             writer.writerow(datum)
